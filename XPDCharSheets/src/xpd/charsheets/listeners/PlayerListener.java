8cd77f462fb520352761ad784c4efe07e854bbd3
XenosProjectDev <xenosprojectdev@gmail.com>
2013-05-29 11:42:26 -0400

    WOP 6.2
    Issue fixed-
    
    don't call the master class for events. use the sub events or it freaks
    out

--- XPDCharSheets/src/xpd/charsheets/listeners/PlayerListener.java
+++ XPDCharSheets/src/xpd/charsheets/listeners/PlayerListener.java
@@ -4,7 +4,6 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 import xpd.charsheets.CharSheets;
@@ -19,13 +18,14 @@ public class PlayerListener implements Listener {
 	}
 
 	@EventHandler(priority=EventPriority.LOWEST)
-	public void playerEvents(PlayerEvent event){
+	public void playerEvents(PlayerJoinEvent event){
 		
 		Player player = event.getPlayer();
 		
 		if (event instanceof PlayerJoinEvent){
 			
 				Sheet.buildSheet(player);
+				
 		}
 	}
 }
